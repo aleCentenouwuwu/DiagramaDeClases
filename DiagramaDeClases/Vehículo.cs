bc9cc07cb83481ba81dd8bfc5489baa4c21c51c1
AlexaniaCenteno <alexaniapauth@gmail.com>
2024-06-22 12:57:47 -0600

    Agregar archivos de proyecto.

---
+++ DiagramaDeClases/Vehículo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DiagramaDeClases
+{
+    public class Vehículo
+    {
+        string marca;
+        string modelo;
+        int anio;
+
+    }
+
+}
---
+++ GestionTiendaEnLinea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTiendaEnLinea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
